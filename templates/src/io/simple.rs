da1cf9dab29d612f86030be0deed841c8207ced8
null_lambda <your_donotreply_email_id>
2024-10-12 17:22:59 +0900

    less memory leak

--- templates/src/io/simple.rs
+++ templates/src/io/simple.rs
@@ -1,9 +1,12 @@
 mod simple_io {
-    pub struct InputAtOnce(std::str::SplitAsciiWhitespace<'static>);
+    pub struct InputAtOnce<'a> {
+        _buf: String,
+        iter: std::str::SplitAsciiWhitespace<'a>,
+    }
 
-    impl InputAtOnce {
-        pub fn token(&mut self) -> &str {
-            self.0.next().unwrap_or_default()
+    impl<'a> InputAtOnce<'a> {
+        pub fn token(&mut self) -> &'a str {
+            self.iter.next().unwrap_or_default()
         }
 
         pub fn value<T: std::str::FromStr>(&mut self) -> T
@@ -14,10 +17,10 @@ mod simple_io {
         }
     }
 
-    pub fn stdin_at_once() -> InputAtOnce {
-        let buf = std::io::read_to_string(std::io::stdin()).unwrap();
-        let buf = Box::leak(buf.into_boxed_str());
-        let iter = buf.split_ascii_whitespace();
-        InputAtOnce(iter)
+    pub fn stdin_at_once<'a>() -> InputAtOnce<'a> {
+        let _buf = std::io::read_to_string(std::io::stdin()).unwrap();
+        let iter = _buf.split_ascii_whitespace();
+        let iter = unsafe { std::mem::transmute(iter) };
+        InputAtOnce { _buf, iter }
     }
 }
