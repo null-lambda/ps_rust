f39cc35ffa9f6d80f0d62d94024b687bb651e27c
null_lambda <your_donotreply_email_id>
2024-10-27 07:11:37 +0900

    update basic template

--- main.rs
+++ main.rs
@@ -1,4 +1,3 @@
-use std::collections::{BTreeMap, BTreeSet};
 use std::io::Write;
 
 mod simple_io {
@@ -21,175 +20,40 @@ mod simple_io {
     }
 
     pub fn stdin_at_once<'a>() -> InputAtOnce<'a> {
-        let buf = std::io::read_to_string(std::io::stdin()).unwrap();
-        let iter = buf.split_ascii_whitespace();
+        let _buf = std::io::read_to_string(std::io::stdin()).unwrap();
+        let iter = _buf.split_ascii_whitespace();
         let iter = unsafe { std::mem::transmute(iter) };
-        InputAtOnce { _buf: buf, iter }
+        InputAtOnce { _buf, iter }
+    }
+
+    pub fn stdout() -> std::io::BufWriter<std::io::Stdout> {
+        std::io::BufWriter::new(std::io::stdout())
     }
 }
 
-#[allow(unused)]
-pub mod splay_tree {
-    use std::{cell::UnsafeCell, cmp::Ordering, mem};
+fn dijkstra(nieghbors: &Vec<Vec<(u32, u32)>>, start: u32) -> Vec<u32> {
+    let mut dist = vec![i32::MAX as u32; nieghbors.len()];
+    let mut pq: BinaryHeap<_> = [Reverse((0, start))].into();
+    dist[start as usize] = 0;
 
-    #[derive(Debug, Clone, Copy)]
-    enum Branch {
-        Left = 0,
-        Right = 1,
-    }
-
-    impl Branch {
-        fn inv(self) -> Self {
-            match self {
-                Branch::Left => Branch::Right,
-                Branch::Right => Branch::Left,
+    while let Some(Reverse((d, d_u))) = pq.pop() {
+        if dist[d_u as usize] < d {
+            continue;
+        }
+        for &(v, d_uv) in &nieghbors[d_u as usize] {
+            let d_v_new = d + d_uv;
+            if dist[v as usize] > d_v_new {
+                dist[v as usize] = d_v_new;
+                pq.push(Reverse((d_v_new, v)));
             }
         }
     }
-
-    #[derive(Debug)]
-    struct Node<K> {
-        key: K,
-        children: [Option<Box<Node<K>>>; 2],
-    }
-
-    impl<K: Ord> Node<K> {
-        fn new(key: K) -> Self {
-            Self {
-                key,
-                children: [None, None],
-            }
-        }
-
-        fn rotate(self: &mut Box<Self>, branch: Branch) {
-            let mut sub: Box<Self> = self.children[branch.inv() as usize].take().unwrap();
-            self.children[branch.inv() as usize] = sub.children[branch as usize].take();
-            std::mem::swap(self, &mut sub);
-            self.children[branch as usize] = Some(sub);
-        }
-
-        fn splay(self: &mut Box<Self>, key: K) {
-            let mut new_children = [None, None];
-            let mut cursors = unsafe {
-                [
-                    &mut *(&mut new_children[0] as *mut _),
-                    &mut *(&mut new_children[1] as *mut _),
-                ]
-            };
-
-            loop {
-                let branch = match key.cmp(&self.key) {
-                    Ordering::Equal => break,
-                    Ordering::Less => Branch::Left,
-                    Ordering::Greater => Branch::Right,
-                };
-
-                let (mut cursor, mut cursor_inv) = cursors.split_at_mut(1);
-                if matches!(branch, Branch::Left) {
-                    mem::swap(&mut cursor, &mut cursor_inv);
-                }
-                let (mut cursor, mut cursor_inv) = (&mut cursor[0], &mut cursor_inv[0]);
-
-                match self.children[branch as usize].take() {
-                    None => break,
-                    Some(child) => {
-                        if key.cmp(&child.key) == Ordering::Less {
-                            self.rotate(branch.inv());
-                            if self.children[branch as usize].is_none() {
-                                break;
-                            }
-                        }
-
-                        **cursor_inv = Some(mem::replace(self, child));
-                        *cursor_inv = &mut cursor_inv.as_mut().unwrap().children[branch as usize];
-                    }
-                }
-            }
-
-            mem::swap(cursors[0], &mut self.children[0]);
-            mem::swap(cursors[1], &mut self.children[1]);
-            self.children = new_children;
-        }
-        /*
-                splay* Splay(int key, splay* root)
-        {
-            if(!root)
-                return NULL;
-            splay header;
-            /* header.rchild points to L tree; header.lchild points to R Tree */
-            header.lchild = header.rchild = NULL;
-            splay* LeftTreeMax = &header;
-            splay* RightTreeMin = &header;
-
-            /* loop until root->lchild == NULL || root->rchild == NULL; then break!
-               (or when find the key, break too.)
-             The zig/zag mode would only happen when cannot find key and will reach
-             null on one side after RR or LL Rotation.
-             */
-            while(1)
-            {
-                if(key < root->key)
-                {
-                    if(!root->lchild)
-                        break;
-                    if(key < root->lchild->key)
-                    {
-                        root = RR_Rotate(root); /* only zig-zig mode need to rotate once,
-                                                   because zig-zag mode is handled as zig
-                                                   mode, which doesn't require rotate,
-                                                   just linking it to R Tree */
-                        if(!root->lchild)
-                            break;
-                    }
-                    /* Link to R Tree */
-                    RightTreeMin->lchild = root;
-                    RightTreeMin = RightTreeMin->lchild;
-                    root = root->lchild;
-                    RightTreeMin->lchild = NULL;
-                }
-                else if(key > root->key)
-                {
-                    if(!root->rchild)
-                        break;
-                    if(key > root->rchild->key)
-                    {
-                        root = LL_Rotate(root);/* only zag-zag mode need to rotate once,
-                                                  because zag-zig mode is handled as zag
-                                                  mode, which doesn't require rotate,
-                                                  just linking it to L Tree */
-                        if(!root->rchild)
-                            break;
-                    }
-                    /* Link to L Tree */
-                    LeftTreeMax->rchild = root;
-                    LeftTreeMax = LeftTreeMax->rchild;
-                    root = root->rchild;
-                    LeftTreeMax->rchild = NULL;
-                }
-                else
-                    break;
-            }
-            /* assemble L Tree, Middle Tree and R tree together */
-            LeftTreeMax->rchild = root->lchild;
-            RightTreeMin->lchild = root->rchild;
-            root->lchild = header.rchild;
-            root->rchild = header.lchild;
-
-            return root;
-        }
-                * */
-    }
-
-    struct SplayTree<K> {
-        root: UnsafeCell<Option<Box<Node<K>>>>,
-    }
-
-    impl<K: Ord> SplayTree<K> {
-        //
-    }
+    dist
 }
 
 fn main() {
     let mut input = simple_io::stdin_at_once();
-    let mut output = std::io::BufWriter::new(std::io::stdout().lock());
+    let mut output = simple_io::stdout();
+
+    let n: usize = input.value();
 }
