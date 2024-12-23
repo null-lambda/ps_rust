725eaeddb0fbd4c59fcf0f91a05519fa4c562cbd
null_lambda <your_donotreply_email_id>
2024-12-23 15:41:08 +0900

    cite

--- solutions/dp/15940_rerooting_lazy.rs
+++ solutions/dp/15940_rerooting_lazy.rs
@@ -1,8 +1,41 @@
+use std::io::Write;
+
+mod simple_io {
+    use std::string::*;
+
+    pub struct InputAtOnce<'a> {
+        _buf: String,
+        iter: std::str::SplitAsciiWhitespace<'a>,
+    }
+
+    impl<'a> InputAtOnce<'a> {
+        pub fn token(&mut self) -> &'a str {
+            self.iter.next().unwrap_or_default()
+        }
+
+        pub fn value<T: std::str::FromStr>(&mut self) -> T
+        where
+            T::Err: std::fmt::Debug,
+        {
+            self.token().parse().unwrap()
+        }
+    }
+
+    pub fn stdin<'a>() -> InputAtOnce<'a> {
+        let _buf = std::io::read_to_string(std::io::stdin()).unwrap();
+        let iter = _buf.split_ascii_whitespace();
+        let iter = unsafe { std::mem::transmute(iter) };
+        InputAtOnce { _buf, iter }
+    }
+
+    pub fn stdout() -> std::io::BufWriter<std::io::Stdout> {
+        std::io::BufWriter::new(std::io::stdout())
+    }
+}
+
 pub mod reroot {
     pub mod lazy {
         // O(n) rerooting dp for trees, with combinable pulling operation. (Monoid action)
-        // https://codeforces.com/blog/entry/124286
-        // https://github.com/koosaga/olympiad/blob/master/Library/codes/data_structures/all_direction_tree_dp.cpp
 
         pub trait RootData {
             type E: Clone; // edge weight
@@ -28,6 +61,7 @@ pub mod reroot {
             data: &[R],
             root: usize,
             yield_node_dp: &mut impl FnMut(usize, R),
+            yield_edge_dp: &mut impl FnMut(&R::F, &R::F, &E),
         ) {
             let n = neighbors.len();
             let mut preorder = vec![];
@@ -58,17 +92,21 @@ pub mod reroot {
             // Reroot
             let mut action_from_parent = vec![R::id_action(); n];
             for &u in &preorder {
+                let (p, w) = &parent[u];
+
                 let mut sum_u = sum_upward[u].clone();
                 sum_u.apply(&action_from_parent[u]);
                 yield_node_dp(u, sum_u.finalize());
+                if u != root {
+                    yield_edge_dp(&action_upward[u], &action_from_parent[u], w);
+                }
 
-                let &(p, _) = &parent[u];
                 let n_child = neighbors[u].len() - (u != root) as usize;
                 match n_child {
                     0 => {}
                     1 => {
                         for (v, w) in &neighbors[u] {
-                            if *v as usize == p {
+                            if *v as usize == *p {
                                 continue;
                             }
                             let exclusive = action_from_parent[u].clone();
@@ -82,7 +120,7 @@ pub mod reroot {
                         let mut prefix: Vec<R::F> = neighbors[u]
                             .iter()
                             .map(|(v, _)| {
-                                if *v as usize == p {
+                                if *v as usize == *p {
                                     action_from_parent[u].clone()
                                 } else {
                                     action_upward[*v as usize].clone()
@@ -90,15 +128,15 @@ pub mod reroot {
                             })
                             .collect();
                         let mut postfix = prefix.clone();
-                        for i in (2..neighbors[u].len()).rev() {
+                        for i in (1..neighbors[u].len()).rev() {
                             postfix[i - 1] = data[u].combine_action(&postfix[i - 1], &postfix[i]);
                         }
-                        for i in 1..neighbors[u].len() - 1 {
+                        for i in 1..neighbors[u].len() {
                             prefix[i] = data[u].combine_action(&prefix[i - 1], &prefix[i]);
                         }
 
                         for (i, (v, w)) in neighbors[u].iter().enumerate() {
-                            if *v as usize == p {
+                            if *v as usize == *p {
                                 continue;
                             }
                             let exclusive = if i == 0 {
@@ -119,4 +157,71 @@ pub mod reroot {
             }
         }
     }
+}
+
+#[derive(Clone, Default, Debug)]
+struct NodeDp {
+    diam: u64,
+    depth: u64,
+}
+
+impl reroot::lazy::RootData for NodeDp {
+    type E = u64;
+    type F = NodeDp;
+
+    fn lift_to_action(&self, weight: &u64) -> NodeDp {
+        NodeDp {
+            diam: self.diam,
+            depth: self.depth + weight,
+        }
+    }
+
+    fn id_action() -> NodeDp {
+        NodeDp { diam: 0, depth: 0 }
+    }
+
+    fn combine_action(&self, lhs: &NodeDp, rhs: &NodeDp) -> NodeDp {
+        let mut res = lhs.clone();
+        res.apply(rhs);
+        res
+    }
+
+    fn apply(&mut self, action: &NodeDp) {
+        self.diam = self.diam.max(action.diam).max(self.depth + action.depth);
+        self.depth = self.depth.max(action.depth);
+    }
+
+    fn finalize(&self) -> NodeDp {
+        Self {
+            diam: self.diam,
+            depth: self.depth,
+        }
+    }
+}
+
+fn main() {
+    let mut input = simple_io::stdin();
+    let mut output = simple_io::stdout();
+
+    let n: usize = input.value();
+    let mut neighbors = vec![vec![]; n];
+    for _ in 0..n - 1 {
+        let a = input.value::<usize>() - 1;
+        let b = input.value::<usize>() - 1;
+        let t: u64 = input.value();
+        neighbors[a].push((b as u32, t));
+        neighbors[b].push((a as u32, t));
+    }
+
+    let mut max_time = 0;
+    reroot::lazy::run(
+        &neighbors,
+        &vec![NodeDp::default(); n],
+        0,
+        &mut |_, _| {},
+        &mut |e1, e2, w| {
+            max_time = max_time.max(e1.diam + e2.diam + w);
+        },
+    );
+    writeln!(output, "{}", max_time).unwrap();
 }
