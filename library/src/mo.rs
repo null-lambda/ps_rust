f03461392803ef36d1dfc2628ffc85a0318737af
null_lambda <your_donotreply_email_id>
2025-01-02 00:56:41 +0900

    customizable map-reducer for segtree

--- library/src/mo.rs
+++ library/src/mo.rs
@@ -1,23 +1,57 @@
-pub mod mo {
+mod mo {
     // Various ordering strategies for Mo's algorithm.
     //
-    // The algorithm has a strong connection to the Traveling Salesman Problem (TSP),
+    // The algorithm has a strong connection to the Traveling Salesman Problem {TSP},
     // as moving from one range query [l, r] to another [l', r'] costs Manhattan distance |l - l'| + |r - r'|.
     // Given a Q random sampled points in a square of side length N,
     // The Beardwood-Halton-Hammersley Theorem establishes an expected minimum path length of O(N sqrt(Q)).
     // Thus, No ordering can escape this asymptotic bound.
 
-    pub fn even_odd_order(n_lattice: usize, n_queries: usize) -> impl Fn(u32, u32) -> (u32, i32) {
-        assert!(n_lattice > 0);
+    use alloc::{vec, vec::Vec};
 
+    fn isqrt(n: u32) -> u32 {
+        if n == 0 {
+            return 0;
+        }
+
+        let mut x = n;
+        loop {
+            let next_x = (x + n / x) / 2;
+            if next_x >= x {
+                return x;
+            }
+            x = next_x;
+        }
+    }
+
+    // Simple while efficient. Requires almost no precomputation cost.
+    pub fn sort_by_even_odd_order(
+        n_lattice: usize,
+        n_queries: usize,
+        intervals: impl IntoIterator<Item = (u32, u32)>,
+    ) -> impl Iterator<Item = (u32, u32, u32)> {
         // Time complexity: T ~ O(Q B + N^2/B)
         // => optimal bucket size: B = N/sqrt(Q), so T ~ O(N sqrt(Q))
-        let bucket_size = (n_lattice as u32 / (n_queries as f64).sqrt().max(1.0) as u32).max(1);
-        move |l, r| {
-            let bl = l / bucket_size;
-            let br = if bl % 2 == 0 { r as i32 } else { -(r as i32) };
-            (bl, br)
-        }
+        let bucket_size = (n_lattice as u32 / isqrt(n_queries as u32) as u32).max(1);
+
+        let flip_by_parity = |bl: u32, r: u32| if bl & 1 == 0 { r } else { !r };
+        let mut res = intervals
+            .into_iter()
+            .enumerate()
+            .map(|(i, (l, r))| {
+                let bl = l / bucket_size;
+                (l, flip_by_parity(bl, r), bl, i as u32)
+            })
+            .collect::<Vec<_>>();
+
+        res.sort_unstable_by(move |x, y| {
+            let (_, rx, blx, _) = x;
+            let (_, ry, bly, _) = y;
+            blx.cmp(&bly).then_with(|| rx.cmp(&ry))
+        });
+
+        res.into_iter()
+            .map(move |(l, r, bl, i)| (l, flip_by_parity(bl, r), i))
     }
 
     // Space-filling curve of TSP for random sampled points.
@@ -42,7 +76,7 @@ pub mod mo {
     pub fn bucketed_hilbert_order(n_lattice: usize, n_queries: usize) -> impl Fn(u32, u32) -> u64 {
         assert!(n_lattice > 0);
 
-        let mut bucket_size = (n_lattice as u32 / (n_queries as f64).sqrt().max(1.0) as u32).max(1);
+        let mut bucket_size = n_lattice as u32 / isqrt(n_queries as u32).max(1);
         bucket_size = bucket_size.next_power_of_two();
         let log2b_ceil = u32::BITS - 1 - bucket_size.leading_zeros();
 
@@ -85,7 +119,7 @@ pub mod mo {
                 1 => (rot + 0) % 4,
                 2 => (rot + 0) % 4,
                 3 => (rot + 1) % 4,
-                _ => unsafe { std::hint::unreachable_unchecked() },
+                _ => unsafe { core::hint::unreachable_unchecked() },
             };
 
             x &= !w_half;
@@ -101,5 +135,159 @@ pub mod mo {
             exp -= 1;
         }
         res
+    }
+
+    // Solve a Traveling Salesperson Problem!
+    // Exact solution with naive dp in O(N^2 2^N)
+    pub fn tsp_naive<const CAP: usize>(
+        xs: impl IntoIterator<Item = (u32, u32)>,
+        x_start: (u32, u32),
+    ) -> Vec<u32> {
+        const INF: u32 = u32::MAX / 3;
+
+        let ps: Vec<_> = xs.into_iter().collect();
+        let n = ps.len();
+
+        assert!(1 <= n && n <= CAP);
+        assert!(CAP <= 16, "N is too large for brute force TSP");
+
+        let metric = |x: &(u32, u32), y: &(u32, u32)| {
+            ((x.0 as i32 - y.0 as i32).abs() + (x.1 as i32 - y.1 as i32).abs()) as u32
+        };
+
+        // dp[visited][last]
+        let n_mask = 1 << n;
+        let mut dp = vec![INF; n_mask * CAP];
+        for last in 0..n {
+            dp[(1 << last) * CAP + last] = metric(&x_start, &ps[last]);
+        }
+
+        // let mut cached_metric: [[_; CAP]; CAP] = [[0; CAP]; CAP];
+        // for i in 0..n {
+        //     for j in 0..n {
+        //         cached_metric[i][j] = metric(&xs[i], &xs[j]);
+        //     }
+        // }
+        // let cached_metric = |x: usize, y: usize| cached_metric[x as usize][y as usize];
+        let metric = |i: usize, j: usize| metric(&ps[i], &ps[j]);
+
+        for visited in 1..n_mask {
+            for last in 0..n {
+                if (visited & (1 << last)) == 0 {
+                    continue;
+                }
+                for prev in 0..n {
+                    if (visited & (1 << prev)) == 0 || prev == last {
+                        continue;
+                    }
+                    let prev_mask = visited ^ (1 << last);
+                    dp[visited * CAP + last] = dp[visited * CAP + last]
+                        .min(dp[prev_mask * CAP + prev] + metric(prev, last));
+                }
+            }
+        }
+
+        let mut min_cost = INF;
+        let mut last = 0;
+        for i in 0..n {
+            if dp[(n_mask - 1) * CAP + i] < min_cost {
+                min_cost = dp[(n_mask - 1) * CAP + i];
+                last = i;
+            }
+        }
+
+        let mut path = vec![0; n];
+        let mut visited = n_mask - 1;
+        'outer: for i in (1..n).rev() {
+            path[i] = last as u32;
+            for prev in 0..n {
+                if (visited & (1 << prev)) != 0
+                    && dp[visited * CAP + last]
+                        == dp[(visited ^ (1 << last)) * CAP + prev] + metric(prev, last)
+                {
+                    visited ^= 1 << last;
+                    last = prev;
+                    continue 'outer;
+                }
+            }
+            panic!("TSP path not found");
+        }
+        path[0] = last as u32;
+        path
+    }
+
+    // Solve a 2-approximation to Manhattan TSP,
+    // by constructing a Manhattan MST and traversing its Euler tour in O(V log V).
+    pub fn tsp_two_approx<T: Clone + Default>(
+        xs: &mut [T],
+        x_start: (u32, u32),
+        mut get_point: impl FnMut(&T) -> (u32, u32),
+    ) {
+        use super::dset::DisjointSet;
+        use super::mst;
+
+        let n = xs.len();
+        let n_verts = n + 1;
+        let root = 0;
+        let ps: Vec<_> = core::iter::once(x_start)
+            .chain(xs.iter().map(|x| get_point(x)))
+            .collect();
+
+        let mut edges = Vec::with_capacity(n_verts * 8 / 2);
+        mst::manhattan_mst_candidates(ps.iter().map(|&(x, y)| (x as i32, y as i32)), |u, v, w| {
+            edges.push((u, v, w));
+        });
+
+        let mut mst_degree = vec![0u32; n_verts];
+        let mut mst_xor_neighbors = vec![0u32; n_verts];
+        mst_degree[root] += 2;
+        mst::filter_kruskal(
+            &mut (n_verts - 1),
+            &mut DisjointSet::new(n_verts),
+            &mut |u, v, _| {
+                mst_degree[u as usize] += 1;
+                mst_degree[v as usize] += 1;
+                mst_xor_neighbors[u as usize] ^= v;
+                mst_xor_neighbors[v as usize] ^= u;
+            },
+            &mut edges,
+        );
+
+        let mut size = vec![1; n_verts];
+        let mut topological_order = Vec::with_capacity(n_verts - 1);
+        for mut u in 0..n_verts as u32 {
+            while mst_degree[u as usize] == 1 {
+                let p = mst_xor_neighbors[u as usize];
+                mst_degree[u as usize] -= 1;
+                mst_degree[p as usize] -= 1;
+                topological_order.push((u, p));
+
+                mst_xor_neighbors[p as usize] ^= u;
+                size[p as usize] += size[u as usize];
+
+                u = p;
+            }
+        }
+        assert_eq!(
+            topological_order.len(),
+            n_verts - 1,
+            "Invalid tree structure"
+        );
+
+        let mut euler_in = size; // 1-based
+        for (u, p) in topological_order.into_iter().rev() {
+            let last_idx = euler_in[p as usize];
+            euler_in[p as usize] -= euler_in[u as usize];
+            euler_in[u as usize] = last_idx;
+        }
+        assert_eq!(euler_in[root], 1);
+
+        let mut res = vec![T::default(); n_verts];
+        for i in 1..n_verts {
+            res[euler_in[i] as usize - 2] = xs[i - 1].clone();
+        }
+        for (i, r) in res.into_iter().enumerate().take(n) {
+            xs[i] = r;
+        }
     }
 }
--- library/src/tree/segtree.rs
+++ library/src/tree/segtree.rs
@@ -1,10 +1,11 @@
 pub mod segtree {
-    use std::{iter, ops::Range};
+    use std::ops::Range;
 
     pub trait Monoid {
         type X;
+        const IS_COMMUTATIVE: bool = false;
         fn id(&self) -> Self::X;
-        fn combine(&self, a: &Self::X, b: &Self::X) -> Self::X;
+        fn op(&self, a: &Self::X, b: &Self::X) -> Self::X;
     }
 
     #[derive(Debug)]
@@ -29,31 +30,24 @@ pub mod segtree {
         pub fn from_iter<I>(iter: I, monoid: M) -> Self
         where
             I: IntoIterator<Item = M::X>,
-            I::IntoIter: ExactSizeIterator,
+            I::IntoIter: ExactSizeIterator<Item = M::X>,
         {
             let iter = iter.into_iter();
             let n = iter.len();
-            let mut sum: Vec<_> = (0..n)
-                .map(|_| monoid.id())
-                .chain(iter)
-                .chain(iter::repeat_with(|| monoid.id()))
-                .take(2 * n)
-                .collect();
-            for i in (1..n).rev() {
-                sum[i] = monoid.combine(&sum[i << 1], &sum[i << 1 | 1]);
+            let mut sum: Vec<_> = (0..n).map(|_| monoid.id()).chain(iter).collect();
+            for i in (0..n).rev() {
+                sum[i] = monoid.op(&sum[i << 1], &sum[i << 1 | 1]);
             }
             Self { n, sum, monoid }
         }
 
-        pub fn modify(&mut self, mut idx: usize, f: impl FnOnce(&mut M::X)) {
+        pub fn set(&mut self, mut idx: usize, value: M::X) {
             debug_assert!(idx < self.n);
             idx += self.n;
-            f(&mut self.sum[idx]);
+            self.sum[idx] = value;
             while idx > 1 {
                 idx >>= 1;
-                self.sum[idx] = self
-                    .monoid
-                    .combine(&self.sum[idx << 1], &self.sum[idx << 1 | 1]);
+                self.sum[idx] = self.monoid.op(&self.sum[idx << 1], &self.sum[idx << 1 | 1]);
             }
         }
 
@@ -61,24 +55,56 @@ pub mod segtree {
             &self.sum[idx + self.n]
         }
 
-        pub fn query_range(&self, range: Range<usize>) -> M::X {
+        pub fn mapped_sum_range<N: Monoid>(
+            &self,
+            range: Range<usize>,
+            codomain: &N,
+            morphism: impl Fn(&M::X) -> N::X,
+        ) -> N::X {
             let Range { mut start, mut end } = range;
+            if start >= end {
+                return codomain.id();
+            }
             debug_assert!(start < self.n && end <= self.n);
             start += self.n;
             end += self.n;
-            let (mut result_left, mut result_right) = (self.monoid.id(), self.monoid.id());
-            while start < end {
-                if start & 1 != 0 {
-                    result_left = self.monoid.combine(&result_left, &self.sum[start]);
-                }
-                if end & 1 != 0 {
-                    result_right = self.monoid.combine(&self.sum[end - 1], &result_right);
-                }
-                start = (start + 1) >> 1;
-                end >>= 1;
-            }
 
-            self.monoid.combine(&result_left, &result_right)
+            if N::IS_COMMUTATIVE {
+                let mut result = codomain.id();
+                while start < end {
+                    if start & 1 != 0 {
+                        result = codomain.op(&result, &morphism(&self.sum[start]));
+                    }
+                    if end & 1 != 0 {
+                        result = codomain.op(&morphism(&self.sum[end - 1]), &result);
+                    }
+                    start = (start + 1) >> 1;
+                    end >>= 1;
+                }
+                result
+            } else {
+                let (mut result_left, mut result_right) = (codomain.id(), codomain.id());
+                while start < end {
+                    if start & 1 != 0 {
+                        result_left = codomain.op(&result_left, &morphism(&self.sum[start]));
+                    }
+                    if end & 1 != 0 {
+                        result_right = codomain.op(&morphism(&self.sum[end - 1]), &result_right);
+                    }
+                    start = (start + 1) >> 1;
+                    end >>= 1;
+                }
+                codomain.op(&result_left, &result_right)
+            }
+        }
+    }
+
+    impl<M: Monoid> SegTree<M>
+    where
+        M::X: Clone,
+    {
+        pub fn sum_range(&self, range: Range<usize>) -> M::X {
+            self.mapped_sum_range(range, &self.monoid, |x| x.clone())
         }
     }
 }
