d426d477fb5631c448554fb88042ca7820edc787
null_lambda <your_donotreply_email_id>
2025-01-17 00:16:14 +0900

    trie with generic transition maps

--- library/src/tree/trie.rs
+++ library/src/tree/trie.rs
@@ -1,50 +1,167 @@
-mod trie {
-    // trie for 'A-Z'
-    pub const N_ALPHABET: usize = 26;
-    pub const UNSET: usize = std::usize::MAX;
+pub mod trie {
+    use std::collections::HashMap;
+    use std::hash::Hash;
 
-    #[derive(Debug, Clone)]
-    pub struct Node {
-        pub children: [usize; N_ALPHABET],
-        pub word_idx: usize,
+    pub const UNSET: u32 = !0;
+    pub const UNSET_KEY: u32 = !0 - 2;
+    pub type NodeRef = u32;
+
+    // The most generic one
+    impl<K> TransitionMap for HashMap<K, NodeRef>
+    where
+        K: Eq + Hash,
+    {
+        type Key = K;
+
+        fn empty() -> Self {
+            Default::default()
+        }
+
+        fn get(&self, key: &Self::Key) -> NodeRef {
+            self.get(key).copied().unwrap_or(UNSET)
+        }
+
+        fn insert(&mut self, key: K, value: NodeRef) {
+            self.insert(key, value);
+        }
     }
 
-    impl Node {
-        fn new() -> Self {
-            Self {
-                children: [UNSET; N_ALPHABET],
-                word_idx: UNSET,
+    // Fixed-size array map
+    impl<const N_ALPHABETS: usize> TransitionMap for [NodeRef; N_ALPHABETS] {
+        type Key = usize;
+
+        fn empty() -> Self {
+            std::array::from_fn(|_| Default::default())
+        }
+
+        fn get(&self, key: &Self::Key) -> NodeRef {
+            self[*key]
+        }
+
+        fn insert(&mut self, key: usize, value: NodeRef) {
+            self[key] = value;
+        }
+    }
+
+    // Adaptive array map, based on the fact that most nodes are slim.
+    pub enum AdaptiveArrayMap<K, const N_ALPHABETS: usize, const STACK_CAP: usize> {
+        Small([(K, NodeRef); STACK_CAP]),
+        Large(Box<[NodeRef; N_ALPHABETS]>),
+    }
+
+    impl<
+            K: Into<usize> + Copy + Default + Eq,
+            const N_ALPHABETS: usize,
+            const STACK_CAP: usize,
+        > TransitionMap for AdaptiveArrayMap<K, N_ALPHABETS, STACK_CAP>
+    {
+        type Key = K;
+
+        fn empty() -> Self {
+            assert!(1 <= STACK_CAP && STACK_CAP <= N_ALPHABETS);
+            Self::Small(std::array::from_fn(|_| (Default::default(), UNSET)))
+        }
+
+        fn get(&self, key: &Self::Key) -> NodeRef {
+            match self {
+                Self::Small(assoc_list) => assoc_list
+                    .iter()
+                    .find_map(|(k, v)| (*k == *key).then(|| *v))
+                    .unwrap_or(UNSET),
+                Self::Large(array_map) => array_map[(*key).into()],
             }
         }
 
-        pub fn terminal(&self) -> bool {
-            self.word_idx != UNSET
-        }
-    }
+        fn insert(&mut self, key: Self::Key, value: NodeRef) {
+            match self {
+                Self::Small(assoc_list) => {
+                    for (k, v) in assoc_list.iter_mut() {
+                        if *k == key {
+                            *v = value;
+                            return;
+                        } else if *v == UNSET_KEY {
+                            *k = key;
+                            *v = value;
+                            return;
+                        }
+                    }
 
-    #[derive(Debug, Clone)]
-    pub struct Trie {
-        pub nodes: Vec<Node>,
-    }
-
-    impl Trie {
-        pub fn new() -> Self {
-            Self {
-                nodes: vec![Node::new()],
-            }
-        }
-
-        pub fn insert(&mut self, s: &[u8], idx: usize) {
-            let mut current = 0;
-            for &c in s {
-                let c = (c - b'A') as usize;
-                if self.nodes[current].children[c] == UNSET {
-                    self.nodes.push(Node::new());
-                    self.nodes[current].children[c] = self.nodes.len() - 1;
+                    let mut array_map = Box::new([UNSET; N_ALPHABETS]);
+                    for (k, v) in assoc_list {
+                        array_map[(*k).into()] = *v;
+                    }
+                    array_map[key.into()] = value;
+                    *self = Self::Large(array_map);
                 }
-                current = self.nodes[current].children[c];
+                Self::Large(array_map) => {
+                    array_map[key.into()] = value;
+                }
             }
-            self.nodes[current].word_idx = idx;
+        }
+    }
+
+    // Generic interface for different associative containers
+    pub trait TransitionMap {
+        type Key;
+        fn empty() -> Self;
+        fn get(&self, key: &Self::Key) -> NodeRef;
+        fn insert(&mut self, key: Self::Key, value: NodeRef);
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct Node<M> {
+        pub children: M,
+        pub tag: u32,
+    }
+
+    #[derive(Debug)]
+    pub struct Trie<M> {
+        pub pool: Vec<Node<M>>,
+    }
+
+    impl<M: TransitionMap> Trie<M> {
+        pub fn new() -> Self {
+            let root = Node {
+                children: M::empty(),
+                tag: UNSET,
+            };
+            Self { pool: vec![root] }
+        }
+
+        fn alloc(&mut self) -> NodeRef {
+            let idx = self.pool.len() as u32;
+            self.pool.push(Node {
+                children: M::empty(),
+                tag: UNSET,
+            });
+            idx
+        }
+
+        pub fn insert(&mut self, path: impl IntoIterator<Item = M::Key>) -> NodeRef {
+            let mut u = 0;
+            for c in path {
+                let next = self.pool[u as usize].children.get(&c);
+                if next == UNSET {
+                    let new_node = self.alloc();
+                    self.pool[u as usize].children.insert(c, new_node);
+                    u = new_node;
+                } else {
+                    u = next;
+                }
+            }
+            u
+        }
+
+        pub fn find(&mut self, path: impl IntoIterator<Item = M::Key>) -> Option<NodeRef> {
+            let mut u = 0;
+            for c in path {
+                let next = self.pool[u as usize].children.get(&c);
+                if next == UNSET {
+                    return None;
+                }
+                u = next;
+            }
+            Some(u)
         }
     }
 }
