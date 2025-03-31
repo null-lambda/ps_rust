2942a10aed138cbe836cff0b72e80d6b0b9ccdda
null_lambda <your_donotreply_email_id>
2025-03-31 13:02:18 +0900

    some number theory

---
+++ library/src/math/factorial.rs
@@ -0,0 +1,44 @@
+fn gen_factorials<T: algebra::Field + Clone + From<u32> + std::fmt::Debug>(
+    n_bound: u32,
+) -> (Vec<T>, Vec<T>) {
+    assert!(n_bound >= 1);
+
+    let mut fac = vec![T::one()];
+    for i in 1..=n_bound {
+        fac.push(fac[i as usize - 1].clone() * T::from(i));
+    }
+
+    let ifac = fac.iter().map(|x| x.inv()).collect();
+
+    // let mut ifac = vec![T::one(); n_bound as usize + 1];
+    // ifac[n_bound as usize] = fac[n_bound as usize].inv();
+    // for i in (2..=n_bound).rev() {
+    //     ifac[i as usize - 1] = ifac[i as usize].clone() * T::from(i);
+    // }
+
+    (fac, ifac)
+}
+
+fn comb_lucas<'a, T: algebra::Field + Clone + std::fmt::Debug>(
+    p: u64,
+    fac_mod_p: &'a [T],
+    ifac_mod_p: &'a [T],
+) -> impl 'a + Fn(u64, u64) -> T {
+    move |mut m: u64, mut n: u64| {
+        let mut acc = T::one();
+        while m > 0 {
+            let x = m % p;
+            let y = n % p;
+            if x < y {
+                return T::zero();
+            }
+            acc *= fac_mod_p[x as usize].clone()
+                * &ifac_mod_p[y as usize]
+                * &ifac_mod_p[(x - y) as usize];
+
+            m /= p;
+            n /= p;
+        }
+        acc
+    }
+}
--- library/src/math/prime.rs
+++ library/src/math/prime.rs
@@ -19,3 +19,71 @@ fn linear_sieve(n_max: u32) -> (Vec<u32>, Vec<u32>) {
 
     (min_prime_factor, primes)
 }
+
+fn gen_euler_phi(min_prime_factor: &[u32]) -> Vec<u32> {
+    let n_bound = min_prime_factor.len();
+    let mut phi = vec![0; n_bound];
+    phi[1] = 1;
+    for i in 2..n_bound {
+        let p = min_prime_factor[i as usize];
+        phi[i] = if p == 0 {
+            i as u32 - 1
+        } else {
+            let m = i as u32 / p;
+            phi[m as usize] * if m % p == 0 { p } else { p - 1 }
+        };
+    }
+    phi
+}
+
+fn gen_mobius(min_prime_factor: &[u32]) -> Vec<i32> {
+    let n_max = min_prime_factor.len() - 1;
+    let mut mu = vec![0; n_max + 1];
+    mu[1] = 1;
+
+    for i in 2..=n_max {
+        let p = min_prime_factor[i];
+        if p == 0 {
+            mu[i] = -1;
+        } else {
+            let m = i as u32 / p;
+            mu[i] = if m % p == 0 { 0 } else { -mu[m as usize] };
+        }
+    }
+    mu
+}
+
+fn factorize(n: u32, min_prime_factor: &[u32]) -> Vec<(u32, u8)> {
+    let mut factors = Vec::new();
+    let mut x = n;
+    while x > 1 {
+        let p = min_prime_factor[x as usize];
+        if p == 0 {
+            factors.push((x as u32, 1));
+            break;
+        }
+        let mut exp = 0;
+        while x % p == 0 {
+            exp += 1;
+            x /= p;
+        }
+        factors.push((p, exp));
+    }
+
+    factors
+}
+
+fn for_each_divisor(factors: &[(u32, u8)], mut visitor: impl FnMut(u32)) {
+    let mut stack = vec![(1, 0u32)];
+    while let Some((mut d, i)) = stack.pop() {
+        if i as usize == factors.len() {
+            visitor(d);
+        } else {
+            let (p, exp) = factors[i as usize];
+            for _ in 0..=exp {
+                stack.push((d, i + 1));
+                d *= p;
+            }
+        }
+    }
+}
