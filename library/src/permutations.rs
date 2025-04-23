aee40ac6ba00143f567c323b0a33d036fba9a2ab
null_lambda <your_donotreply_email_id>
2025-04-23 19:53:59 +0900

    gospher's trick for bitmasked combinations

--- library/src/permutations.rs
+++ library/src/permutations.rs
@@ -12,3 +12,23 @@ fn next_permutation<T: Ord>(arr: &mut [T]) -> bool {
         }
     }
 }
+
+fn combinations_u32(n: usize, r: usize) -> impl Iterator<Item = u32> {
+    assert!(n <= 31);
+    std::iter::successors(Some((1u32 << r) - 1), move |&mask| {
+        let mut next = mask;
+        let lsb = next & next.wrapping_neg();
+        let r = next + lsb;
+        if r >= 1 << n {
+            return None;
+        }
+        next = (((next ^ r) >> 2) / lsb) | r;
+        Some(next)
+    })
+}
+
+fn nonzero_bits_u32(n: u32) -> impl Iterator<Item = u32> {
+    std::iter::successors(Some(n), |&mask| Some(mask & (mask - 1)))
+        .take_while(|&mask| mask != 0)
+        .map(|mask| u32::BITS - 1 - (mask & mask.wrapping_neg()).leading_zeros())
+}
