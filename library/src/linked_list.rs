7bf84a2c33f0b5c5f7241213b1aa32446eaf2123
null_lambda <your_donotreply_email_id>
2025-09-12 13:31:27 +0900

    linked list

--- library/src/linked_list.rs
+++ library/src/linked_list.rs
@@ -1,5 +1,6 @@
 mod linked_list {
-    // Arena-allocated pool of cyclic doubly linked lists.
+    // Arena-allocated pool of cyclic doubly linked lists,
+    // represented by a directed permutation graph.
     #[derive(Clone, Debug)]
     pub struct CyclicListPool {
         links: Vec<[u32; 2]>,
@@ -12,12 +13,16 @@ mod linked_list {
             }
         }
 
-        pub fn is_isolated(&self, u: u32) -> bool {
-            self.links[u as usize][0] == u
+        pub fn links(&self, u: u32) -> [u32; 2] {
+            self.links[u as usize]
         }
 
-        pub fn get_links(&self, u: u32) -> [u32; 2] {
-            self.links[u as usize]
+        pub fn next(&self, u: u32) -> u32 {
+            self.links[u as usize][1]
+        }
+
+        pub fn is_isolated(&self, u: u32) -> bool {
+            self.links[u as usize][0] == u
         }
 
         pub fn isolate(&mut self, u: u32) {
@@ -50,6 +55,14 @@ mod linked_list {
             self.links[pivot as usize][0] = u;
         }
 
+        pub fn insert_right(&mut self, pivot: u32, u: u32) {
+            debug_assert!(self.is_isolated(u));
+            let [_, a] = self.links[pivot as usize];
+            self.links[pivot as usize][1] = u;
+            self.links[u as usize] = [pivot, a];
+            self.links[a as usize][0] = u;
+        }
+
         pub fn split_slice_out(&mut self, head: u32, tail: u32) {
             debug_assert!(self.is_connected(head, tail));
 
@@ -63,6 +76,36 @@ mod linked_list {
             self.links[b as usize][0] = a;
             self.links[tail as usize][1] = head;
             self.links[head as usize][0] = tail;
+        }
+
+        pub fn pop_left(&mut self, u: u32) -> Option<u32> {
+            if self.is_isolated(u) {
+                return None;
+            }
+
+            let [a, _] = self.links[u as usize];
+            let [b, _] = self.links[a as usize];
+
+            self.links[b as usize][1] = u;
+            self.links[a as usize] = [a; 2];
+            self.links[u as usize][0] = b;
+
+            Some(a)
+        }
+
+        pub fn pop_right(&mut self, u: u32) -> Option<u32> {
+            if self.is_isolated(u) {
+                return None;
+            }
+
+            let [_, a] = self.links[u as usize];
+            let [_, b] = self.links[a as usize];
+
+            self.links[u as usize][1] = b;
+            self.links[a as usize] = [a; 2];
+            self.links[b as usize][0] = u;
+
+            Some(a)
         }
     }
 }
