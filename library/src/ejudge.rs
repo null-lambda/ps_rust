5ed95ea41e0de46a7a95b809c5d5f2b678d5bdbc
null-lambda <jeoffrey0522@gmail.com>
2026-01-20 22:01:04 +0900

    join

--- library/src/ejudge.rs
+++ library/src/ejudge.rs
@@ -1,7 +1,16 @@
 fn yesno(b: bool) -> &'static str {
-    if b {
-        "Yes"
-    } else {
-        "No"
+    if b { "Yes" } else { "No" }
+}
+
+fn join<'a>(iter: impl 'a + IntoIterator<Item = impl std::fmt::Display>) -> String {
+    use std::fmt::Write;
+    let mut iter = iter.into_iter();
+    let mut res = String::new();
+    if let Some(x) = iter.next() {
+        write!(&mut res, "{x}").unwrap();
+        for x in iter {
+            write!(&mut res, " {x}").unwrap();
+        }
     }
+    res
 }
