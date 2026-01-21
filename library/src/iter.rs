67d881897f0ea0e10663b6c6d44c7350b656b83f
null-lambda <jeoffrey0522@gmail.com>
2026-01-22 00:17:27 +0900

    extended traits

--- library/src/iter.rs
+++ library/src/iter.rs
@@ -1,24 +1,117 @@
-pub mod iter {
-    fn accumulate<T: Clone>(
-        iter: impl IntoIterator<Item = T>,
-        init: T,
-        mut f: impl FnMut(T, T) -> T,
-    ) -> impl Iterator<Item = T> {
-        std::iter::once(init.clone()).chain(iter.into_iter().scan(init, move |acc, x| {
-            *acc = f(acc.clone(), x);
-            Some(acc.clone())
-        }))
+trait IteratorExt: Iterator {
+    fn intersp_with(self, mut sep: impl FnMut() -> Self::Item) -> impl Iterator<Item = Self::Item>
+    where
+        Self: Sized,
+    {
+        let mut this = self.peekable();
+        let mut yield_sep = true;
+        std::iter::from_fn(move || {
+            yield_sep ^= true;
+            if this.peek().is_none() {
+                None
+            } else if yield_sep {
+                Some(sep())
+            } else {
+                this.next()
+            }
+        })
     }
 
-    pub fn product<I, J>(i: I, j: J) -> impl Iterator<Item = (I::Item, J::Item)>
+    fn intersp(self, sep: Self::Item) -> impl Iterator<Item = Self::Item>
     where
-        I: IntoIterator,
-        I::Item: Clone,
-        J: IntoIterator,
-        J::IntoIter: Clone,
+        Self: Sized,
+        Self::Item: Clone,
     {
-        let j = j.into_iter();
-        i.into_iter()
-            .flat_map(move |x| j.clone().map(move |y| (x.clone(), y)))
+        self.intersp_with(move || sep.clone())
     }
+
+    fn join(self) -> String
+    where
+        Self: Sized,
+        Self::Item: std::fmt::Display,
+    {
+        use std::fmt::Write;
+        let mut iter = self.into_iter();
+        let mut res = String::new();
+        if let Some(x) = iter.next() {
+            write!(&mut res, "{x}").unwrap();
+            for x in iter {
+                write!(&mut res, " {x}").unwrap();
+            }
+        }
+        res
+    }
+
+    fn cprod<Other>(self, other: Other) -> impl Iterator<Item = (Self::Item, Other::Item)>
+    where
+        Self: Sized,
+        Self::Item: Clone,
+        Other: IntoIterator,
+        Other::IntoIter: Clone,
+    {
+        let other = other.into_iter();
+        self.flat_map(move |x| other.clone().map(move |y| (x.clone(), y)))
+    }
+
+    // fn cpow(self, exp: usize) -> impl Iterator<Item = Vec<Self::Item>>
+    // where
+    //     Self: Sized,
+    //     Self::Item: Clone,
+    // {
+    //     todo!()
+    // }
+}
+impl<I: Iterator> IteratorExt for I {}
+
+pub trait TuplePush<S> {
+    type Output;
+    fn push(self, x: S) -> Self::Output;
+}
+macro_rules! impl_tuple_ops {
+(@cascade $($t:ident)*) => {
+    impl<$($t,)* S> TuplePush<S> for ($($t,)*) {
+        type Output = ($($t,)* S,);
+        fn push(self, x: S) -> Self::Output {
+            #[allow(non_snake_case)]
+            let ($($t,)*) = self;
+            ($($t,)* x,)
+        }
+    }
+};
+($t:ident $($rest:ident)*) => {
+    impl_tuple_ops!(@cascade $t $($rest)*);
+    impl_tuple_ops!($($rest)*);
+};
+() => {
+    impl_tuple_ops!(@cascade);
+};
+}
+impl_tuple_ops!(T0 T1 T2 T3 T4 T5 T6 T7 T8 T9);
+
+pub trait TupleFlatten {
+    type Output;
+    fn flatten(self) -> Self::Output;
+}
+impl<T> TupleFlatten for (T,) {
+    type Output = (T,);
+    fn flatten(self) -> Self::Output {
+        self
+    }
+}
+impl<T, S> TupleFlatten for (T, S)
+where
+    T: TuplePush<S>,
+{
+    type Output = <T as TuplePush<S>>::Output;
+    fn flatten(self) -> Self::Output {
+        self.0.push(self.1)
+    }
+}
+macro_rules! cprods {
+    ($t:expr) => {
+        $t.map(|t| (t,))
+    };
+    ($t:expr, $($rest:expr),+) => {
+        ($t.map(|t| (t,)) $(.cprod($rest))*).map(TupleFlatten::flatten)
+    };
 }
--- library/src/slice_utils.rs
+++ library/src/slice_utils.rs
@@ -1,60 +1,82 @@
-fn group_indices_by<'a, T>(
-    xs: &'a [T],
-    mut pred: impl 'a + FnMut(&T, &T) -> bool,
-) -> impl 'a + Iterator<Item = [usize; 2]> {
-    let mut i = 0;
-    std::iter::from_fn(move || {
-        if i == xs.len() {
-            return None;
-        }
+pub trait SliceExt<'a, T: 'a> {
+    fn as_slice(&'a self) -> &'a [T];
+    fn as_mut_slice(&'a mut self) -> &'a mut [T];
 
-        let mut j = i + 1;
-        while j < xs.len() && pred(&xs[j - 1], &xs[j]) {
-            j += 1;
-        }
-        let res = [i, j];
-        i = j;
-        Some(res)
-    })
-}
+    fn group_indices_by(
+        &'a self,
+        mut pred: impl 'a + FnMut(&T, &T) -> bool,
+    ) -> impl 'a + Iterator<Item = [usize; 2]> {
+        let xs = self.as_slice();
+        let mut i = 0;
+        std::iter::from_fn(move || {
+            if i == xs.len() {
+                return None;
+            }
 
-fn group_indices_by_key<'a, T, K: PartialEq>(
-    xs: &'a [T],
-    mut key: impl 'a + FnMut(&T) -> K,
-) -> impl 'a + Iterator<Item = [usize; 2]> {
-    group_indices_by(xs, move |a, b| key(a) == key(b))
-}
-
-fn group_indices<'a, T: PartialEq>(xs: &'a [T]) -> impl 'a + Iterator<Item = [usize; 2]> {
-    group_indices_by(xs, |x, y| x == y)
-}
-
-fn group_by<'a, T>(
-    xs: &'a [T],
-    pred: impl 'a + FnMut(&T, &T) -> bool,
-) -> impl 'a + Iterator<Item = &'a [T]> {
-    group_indices_by(xs, pred).map(|w| &xs[w[0]..w[1]])
-}
-
-fn group_by_key<'a, T, K: PartialEq>(
-    xs: &'a [T],
-    mut key: impl 'a + FnMut(&T) -> K,
-) -> impl 'a + Iterator<Item = &'a [T]> {
-    group_by(xs, move |a, b| key(a) == key(b))
-}
-
-fn groups<'a, T: PartialEq>(xs: &'a [T]) -> impl 'a + Iterator<Item = &'a [T]> {
-    group_by(xs, |x, y| x == y)
-}
-
-fn partition_in_place<T>(xs: &mut [T], mut pred: impl FnMut(&T) -> bool) -> (&mut [T], &mut [T]) {
-    let n = xs.len();
-    let mut i = 0;
-    for j in 0..n {
-        if pred(&xs[j]) {
-            xs.swap(i, j);
-            i += 1;
-        }
+            let mut j = i + 1;
+            while j < xs.len() && pred(&xs[j - 1], &xs[j]) {
+                j += 1;
+            }
+            let res = [i, j];
+            i = j;
+            Some(res)
+        })
     }
-    xs.split_at_mut(i)
+    fn group_indices_by_key<K: PartialEq>(
+        &'a self,
+        mut key: impl 'a + FnMut(&T) -> K,
+    ) -> impl 'a + Iterator<Item = [usize; 2]> {
+        self.group_indices_by(move |a, b| key(a) == key(b))
+    }
+    fn group_indices(&'a self) -> impl 'a + Iterator<Item = [usize; 2]>
+    where
+        T: PartialEq,
+    {
+        self.group_indices_by(|x, y| x == y)
+    }
+
+    fn group_by(
+        &'a self,
+        pred: impl 'a + FnMut(&T, &T) -> bool,
+    ) -> impl 'a + Iterator<Item = &'a [T]> {
+        let xs = self.as_slice();
+        self.group_indices_by(pred).map(|w| &xs[w[0]..w[1]])
+    }
+    fn group_by_key<K: PartialEq>(
+        &'a self,
+        mut key: impl 'a + FnMut(&T) -> K,
+    ) -> impl 'a + Iterator<Item = &'a [T]> {
+        self.group_by(move |a, b| key(a) == key(b))
+    }
+    fn groups(&'a self) -> impl 'a + Iterator<Item = &'a [T]>
+    where
+        T: PartialEq,
+    {
+        self.group_by(|x, y| x == y)
+    }
+
+    fn partition_in_place(
+        &'a mut self,
+        mut pred: impl FnMut(&T) -> bool,
+    ) -> (&'a mut [T], &'a mut [T]) {
+        let xs = self.as_mut_slice();
+        let n = xs.len();
+        let mut i = 0;
+        for j in 0..n {
+            if pred(&xs[j]) {
+                xs.swap(i, j);
+                i += 1;
+            }
+        }
+        xs.split_at_mut(i)
+    }
+}
+
+impl<'a, T: 'a> SliceExt<'a, T> for [T] {
+    fn as_slice(&'a self) -> &'a [T] {
+        self
+    }
+    fn as_mut_slice(&'a mut self) -> &'a mut [T] {
+        self
+    }
 }
